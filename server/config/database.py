4d0fdf9aa345ba29c2ca0c4bebf8ecdeee17714d
hardik.singh <hardik.singh@hertshtengroup.com>
2024-11-14 16:01:38 +0530

    added DBNAME

--- server/config/database.py
+++ server/config/database.py
@@ -5,10 +5,11 @@ import os
 load_dotenv()
 
 dbURL = os.getenv("DATABASE_URL")
+dbNAME = os.getenv("DATABASE_NAME")
 # client = MongoClient("mongodb+srv://hardik:hardik@cluster0.tasf6.mongodb.net/?retryWrites=true&w=majority")
 client = MongoClient(dbURL)
 # mongodb://admin.alert:alert123@hor-mongodb1.corp.hertshtengroup.com:27017/?authSource=alert
 
-db = client.alert
+db = client[dbNAME]
 
 subscriptions = db["subscriptions"] 
