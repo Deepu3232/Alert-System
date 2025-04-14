55334d2949f911a87f1bb8031c32e587e187ae46
Srikanth Muthyala <srikanth.muthyala@hertshtengroup.com>
2025-04-14 10:31:44 +0000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
     port: 3000,
     strictPort: true,
     host: true,
-    origin: "http://localhost:3000",#test
+    origin: "http://localhost:3000",
     allowedHosts: ["hydalert.dev.hertshtengroup.com"]
   },
 })
